308c43d6e54ae71b4a673b8306f7ffc6ab4c26d9
GOPAL THAKUR <gopalk557@gmail.com>
2022-02-11 02:01:43 +0530

    [Gopal Thakur]Add:Two Lines Equality

--- LineComparisonProblem/CalculateLineLength.cs
+++ LineComparisonProblem/CalculateLineLength.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LineComparisonProblem
 {
-    internal class CalculateLineLength
+    public class CalculateLineLength
     {
         int x1, y1, x2, y2;
         public CalculateLineLength(int x1, int y1, int x2, int y2)
@@ -20,8 +20,23 @@ namespace LineComparisonProblem
         {
             double Length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             Console.WriteLine(Length);
+        
+
+            int x1, y1, x2, y2;
+            public CalculateLineLength(int x1, int y1, int x2, int y2)
+            {
+                this.x1 = x1;
+                this.y1 = y1;
+                this.x2 = x2;
+                this.y2 = y2;
+            }
+            public void CalculateLength()
+            {
+                double Length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+                // Console.WriteLine(Length);
 
 
+            }
         }
 
     }
